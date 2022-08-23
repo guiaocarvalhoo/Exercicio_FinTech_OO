f75c9aca8a4ac0b30503479b22a7ee4e17d0483b
Guilherme Augusto de Oliveira Carvalho <77821746+guiaocarvalhoo@users.noreply.github.com>
2022-08-23 16:27:32 -0300

    Inserindo arquivos de projeto
    Projetos realizados em C# de Orientação a Objetos.

---
+++ Projetos_Orientação_Objeto/Fiap.Aula02/Fiap.Exercicio/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Exercicio.Models
+{
+    internal class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+
+        public Cliente(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+}
